c8ddaf99fd73c9a3c66011b5c80e4690cf1b197d
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-01-26 01:00:30 +0300

    1 ile 100 arasindeki asal sayilari bastiran program

---
+++ Donguler/Fibonacci.java
@@ -0,0 +1,26 @@
+package patikadevOdevlerDonguler;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int number, nmbr1=0, nmbr2=1;
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		number = scn.nextInt();
+		
+		if(number>2) {
+			// System.out.print("0 ");
+			for(int i=1; i<=number;i++) {
+				System.out.print(nmbr1+" ");
+				int tmp=nmbr1;
+				nmbr1=nmbr2; 
+				nmbr2+=tmp; 
+			}
+		}
+
+	}
+
+}
