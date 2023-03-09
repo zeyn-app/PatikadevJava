d65b5d44e63afad9cc3538ef63c453e19dfa5999
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-03-09 21:44:09 +0300

    Print B with stars.

--- Diziler/LettersWithStars.java
+++ Diziler/LettersWithStars.java
@@ -3,17 +3,18 @@ package Arrays;
 public class LettersWithStars {
 
 	 public static void main(String[] args) {
-	        String[][] letter = new String[7][4];
+	        String[][] letter = new String[9][4];
 
 	        for (int i = 0; i < letter.length; i++) {
 	            for (int j = 0; j < letter[i].length; j++) {
-	                if (i == 0 || i == 3  || i == 6) {
+	                if (i == 0 || i == 4  || i == 8) {
 	                    letter[i][j] = " * ";
 	                } else if (j == 0 || j == 3) {
 	                    letter[i][j] = " * ";
 	                } else {
 	                    letter[i][j] = "   ";
 	                }
+	                if((i == 4 && j == 3)) letter[i][j] = "* ";
 	            }
 	        }
 
