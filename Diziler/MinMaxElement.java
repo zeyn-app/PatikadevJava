2c54a0a16a7269442f2acbca81d73bd134902a43
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-03-21 23:15:18 +0300

    It is updated.

--- Diziler/MinMaxElement.java
+++ Diziler/MinMaxElement.java
@@ -1,29 +1,53 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MinMaxElement {
 
 	public static void main(String[] args) {
 		int[] list = { 56, 34, 1, 8, 101, -2, -33 };
+		
+		System.out.println("Enter the number: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
 
-		String min = "Minimum number is ";
-		String max = "Maximum number is ";
-		print(min, findMin(list));
-		print(max, findMax(list));
+		String min = "The nearest number that greater than entered number ";
+		String max = "The nearest number that smaller than entered number ";
+
+		
+		print(min, findMin(list, number));
+		print(max, findMax(list, number));
+		
+		/*Arrays.sort(list);
+		
+		for (int i : list) {
+			System.out.print(i + " ");
+		}*/
 
 	}
 
-	public static int findMin(int[] arr) {
+	public static int findMin(int[] arr, int number) {
 		Arrays.sort(arr);
-		return arr[0];
+		for(int i=arr.length-1; i>=0; i--) {
+			if(arr[i]<number) {
+				return arr[i];
+			}
+		}
+		return -1;
 	}
 
-	public static int findMax(int[] arr) {
+	public static int findMax(int[] arr, int number) {
 		int size = arr.length;
 		Arrays.sort(arr);
+		
+		for(int i=0; i<size; i++) {
+			if(arr[i]>number) {
+				return arr[i];
+			}
+		}
 
-		return arr[size - 1];
+		return -1;
 	}
 
 	public static void print(String str, int number) {
